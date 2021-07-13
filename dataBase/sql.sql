650b350aad6e5289a8030f85a6d39e3ab0e1700a
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-07-13 15:45:02 -0300

    adiciona tabela do banco de dados

--- dataBase/sql.sql
+++ dataBase/sql.sql
@@ -1,6 +1,16 @@
+create database quizGame;
+
 CREATE TABLE users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    nickname VARCHAR(20),
+    avatar VARCHAR(100)
 );
+
+select *from users;
+desc users;
+truncate table users;
+drop table users;
+
+drop database quizGame;
